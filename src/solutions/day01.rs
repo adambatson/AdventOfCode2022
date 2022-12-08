4b25e32942390afe9a66af83bff4cebc0fcfa51a
Adam J Batson <adamjbatson@gmail.com>
2022-12-07 21:03:16 -0500

    Solve Day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,4 +1,33 @@
 
+const INPUT: &'static str = include_str!("../../inputs/day01.txt");
+
 pub fn solve() {
-    println!("Solving Day 01!");
+    println!("Solving Day 01!\n");
+
+    let calories_vec: Vec<Vec<u32>> = INPUT
+        .split("\n\n")
+        .map(|s| {
+            s.split('\n')
+                .map(|num| {
+                    num.parse::<u32>()
+                        .unwrap_or_else(|v| panic!("Num is invalid: {}", v))
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut sum_calories : Vec<u32> = calories_vec.iter().map(|c| c.iter().sum()).collect();
+
+    sum_calories.sort_by(|a, b| b.cmp(a));
+
+    println!("Part A Solution {}", sum_calories.first().unwrap());
+
+    let mut top_three : u32 = 0;
+
+    for i in 0..3 {
+        top_three += sum_calories[i];
+    }
+
+    println!("\nPart B Solution {}", top_three);
+
 }
