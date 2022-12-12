628c5ca52c45fe46f52003a5fac4160faac2fe6c
Adam J Batson <adamjbatson@gmail.com>
2022-12-12 18:09:41 -0500

    Solve Day 8

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -24,11 +24,13 @@ fn is_visible(grid: &Box<Vec<Vec<usize>>>, i: usize, j: usize) -> bool {
     }
 
     let curr_height = grid[i][j];
+    let height = grid.clone().len();
+    let width = grid[i].clone().len();
     //println!("curr hieght {}", curr_height);
     // move "up"
     let up_res: usize = (0..i).into_iter()
         .map(|idx| grid[idx][j])
-        .filter(|val| val > &curr_height)
+        .filter(|val| val >= &curr_height)
         .collect::<Vec<usize>>()
         .len();
     if up_res == 0 {
@@ -36,7 +38,7 @@ fn is_visible(grid: &Box<Vec<Vec<usize>>>, i: usize, j: usize) -> bool {
     }
 
     // move "down"
-    let down_res: usize = (i+1..grid.len()).into_iter()
+    let down_res: usize = (i+1..height).into_iter()
         .map(|idx| grid[idx][j])
         .filter(|val| val >= &curr_height)
         .collect::<Vec<usize>>()
@@ -56,7 +58,7 @@ fn is_visible(grid: &Box<Vec<Vec<usize>>>, i: usize, j: usize) -> bool {
     }
 
     // move "right"
-    let right_res: usize = (j+1..grid[i].len()).into_iter()
+    let right_res: usize = (j+1..width).into_iter()
         .map(|jdx| grid[i][jdx])
         .filter(|val| val >= &curr_height)
         .collect::<Vec<usize>>()
@@ -66,6 +68,55 @@ fn is_visible(grid: &Box<Vec<Vec<usize>>>, i: usize, j: usize) -> bool {
     }
     
     return false;
+}
+
+fn calc_scenic_score(grid: &Box<Vec<Vec<usize>>>, i: usize, j: usize) -> usize {
+    let curr_height = grid[i][j];
+
+    // up score
+    let mut up: usize = 0;
+    if i != 0 {
+        for idx in (0..i).rev() {
+            up += 1;
+            if grid[idx][j] >= curr_height {
+                break;
+            }
+        }
+    }
+    // down score
+    let mut down: usize = 0;
+    if i != grid.len() {
+        for idx in i+1..grid.len() {
+            down += 1;
+            if grid[idx][j] >= curr_height {
+                break;
+            }
+        }
+    }
+    // left score
+    let mut left: usize = 0;
+    if j != 0 {
+        for jdx in (0..j).rev() {
+            left += 1;
+            if grid[i][jdx] >= curr_height {
+                break;
+            }
+        }
+    }
+    // right score
+    let mut right: usize = 0;
+    if j != grid[i].len() {
+        for jdx in j+1..grid[i].len() {
+            right += 1;
+            if grid[i][jdx] >= curr_height {
+                break;
+            }
+        }
+    }
+
+    //println!("{} {} {} {}", up, down, left, right);
+
+    return up * down * left * right;
 }
 
 fn solve_part_a(input: &'static str) -> usize {
@@ -90,7 +141,7 @@ fn solve_part_a(input: &'static str) -> usize {
         for j in 0..max_j {
             if is_visible(&grid_box, i, j) {
                 if i > 0 && j > 0 && i < max_i - 1 && j < max_j - 1 {
-                    println!("{} {} is visible!", i, j);   
+                    //println!("{} {} is visible!", i, j);   
                 }
                 count += 1;
             }
@@ -101,12 +152,37 @@ fn solve_part_a(input: &'static str) -> usize {
 
 }
 
+fn solve_part_b(input: &'static str) -> usize {
+    let grid = parse_grid(input);
+
+    let grid_box = Box::new(grid.to_owned());
+
+    let max_i = grid.clone().len();
+    let max_j = grid[max_i - 1].clone().len();
+
+    //is_visible(&grid_box, 1, 1);
+
+    let mut scenic_scores : Vec<usize> = Vec::new();
+
+    for i in 0..max_i{
+        for j in 0..max_j {
+            scenic_scores.push(calc_scenic_score(&grid_box, i, j));
+        }
+    }
+
+    return scenic_scores.iter().max().unwrap().to_owned();
+}
+
 pub fn solve() {
     println!("Solving Day 08!");
 
     let part_a = solve_part_a(INPUT);
 
     println!("Part A Solution = {}", part_a);
+
+    let part_b = solve_part_b(INPUT);
+
+    println!("Part B Solution = {}", part_b);
 }
 
 #[test]
@@ -192,6 +268,19 @@ pub fn test_is_visible_6() -> Result<(), ()> {
     let grid_box = Box::new(grid.to_owned());
 
     assert_eq!(true, is_visible(&grid_box, 3, 2));
+
+    Ok(())
+}
+
+#[test]
+pub fn test_calc_scenic_score() -> Result<(), ()> {
+    let input: &'static str = include_str!("../../inputs/test/day08.txt");
+
+    let grid = parse_grid(input);
+
+    let grid_box = Box::new(grid.to_owned());
+
+    assert_eq!(8, calc_scenic_score(&grid_box, 3, 2));
 
     Ok(())
 }
